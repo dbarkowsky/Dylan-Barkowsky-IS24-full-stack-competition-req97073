4d387309b49891a9f25f028b40302456b2e612de
Dylan Barkowsky <dylanbarkowsky@gmail.com>
2023-03-25 09:36:18 -0700

    Minor table changes

--- app/components/ProductTable.js
+++ app/components/ProductTable.js
@@ -7,6 +7,15 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
+const HeaderRow = ({ children }) => <TableCell sx={{
+  fontWeight: 600,
+}}>{children}</TableCell>;
+
+const formatDate = (date) => {
+  let formattedDate = new Date(date);
+  return formattedDate.toLocaleDateString();
+}
+
 const ProductTable = ({ data }) => {
   console.log(data);
   return (
@@ -14,13 +23,13 @@ const ProductTable = ({ data }) => {
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Product Number</TableCell>
-            <TableCell align="right">Product Name</TableCell>
-            <TableCell align="right">Scrum Master</TableCell>
-            <TableCell align="right">Product Owner</TableCell>
-            <TableCell align="right">Developers</TableCell>
-            <TableCell align="right">Start Date</TableCell>
-            <TableCell align="right">Methodology</TableCell>
+            <HeaderRow >Product Number</HeaderRow>
+            <HeaderRow >Product Name</HeaderRow>
+            <HeaderRow >Scrum Master</HeaderRow>
+            <HeaderRow >Product Owner</HeaderRow>
+            <HeaderRow >Developers</HeaderRow>
+            <HeaderRow >Start Date</HeaderRow>
+            <HeaderRow >Methodology</HeaderRow>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -32,12 +41,12 @@ const ProductTable = ({ data }) => {
               <TableCell component="th" scope="row">
                 {row.productId}
               </TableCell>
-              <TableCell align="right">{row.productName}</TableCell>
-              <TableCell align="right">{row.scrumMasterName}</TableCell>
-              <TableCell align="right">{row.productOwnerName}</TableCell>
-              <TableCell align="right">{row.developers}</TableCell>
-              <TableCell align="right">{row.startDate}</TableCell>
-              <TableCell align="right">{row.methodology}</TableCell>
+              <TableCell >{row.productName}</TableCell>
+              <TableCell >{row.scrumMasterName}</TableCell>
+              <TableCell >{row.productOwnerName}</TableCell>
+              <TableCell >{row.developers.join(', ')}</TableCell>
+              <TableCell >{formatDate(row.startDate)}</TableCell>
+              <TableCell >{row.methodology}</TableCell>
             </TableRow>
           ))}
         </TableBody>
--- app/pages/index.js
+++ app/pages/index.js
@@ -19,6 +19,21 @@ export default function Home() {
     scrumMasterName: "Garfield",
     startDate: "2018-12-10T13:49:51.141Z",
     methodology: "Agile"
+  },
+  {
+    productId: 2,
+    productName: "Dog Food",
+    productOwnerName: "Rover",
+    developers: [
+      "NAME_1",
+      "NAME_2",
+      "NAME_3",
+      "NAME_4",
+      "NAME_5"
+    ],
+    scrumMasterName: "Lassie",
+    startDate: "2019-12-10T13:49:51.141Z",
+    methodology: "Waterfall"
   }];
 
   const [products, setProducts] = useState(testData);
