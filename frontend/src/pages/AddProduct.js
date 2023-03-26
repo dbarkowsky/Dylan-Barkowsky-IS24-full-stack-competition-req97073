b183dd7c7772058e909b6fa47e99969fd1a77c53
Dylan Barkowsky <dylanbarkowsky@gmail.com>
2023-03-26 00:02:28 -0700

    Added select and chip fields

--- frontend/src/pages/AddProduct.js
+++ frontend/src/pages/AddProduct.js
@@ -1,4 +1,4 @@
-import { FormHelperText, Input, InputLabel, FormControl, MenuItem, Select, Button, Paper } from "@mui/material";
+import { FormHelperText, Chip, Input, InputLabel, FormControl, MenuItem, Select, Button, Paper } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2/Grid2";
 import { useState } from "react";
 
@@ -12,6 +12,10 @@ const AddProduct = () => {
     methodology: ""
   }
   const [formValues, setFormValues] = useState(defaultValues);
+  const [methodology, setMethodology] = useState("");
+  const [developers, setDevelopers] = useState([]);
+  const [currValue, setCurrValue] = useState("");
+
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -21,10 +25,32 @@ const AddProduct = () => {
     });
   };
 
+  const handleChange = (e) => {
+    setCurrValue(e.target.value);
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(e.target.productName.value);
   };
+
+  const handleMethodology = (e) => {
+    setMethodology(e.target.value);
+  }
+
+  const saveValue = (e) => {
+    if (e.keyCode == 32) {
+      setDevelopers((oldState) => [...oldState, e.target.value]);
+      setCurrValue('');
+    }
+  }
+
+  const handleDelete = (item, index) => {
+    let arr = [...developers]
+    arr.splice(index, 1)
+    console.log(item)
+    setDevelopers(arr)
+  }
 
   return (
     <Paper sx={{
@@ -65,25 +91,40 @@ const AddProduct = () => {
               <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
             </FormControl>
           </Grid>
-          <Grid xs={4}>
+          <Grid xs={8}>
             <FormControl sx={{ minWidth: 200 }}>
               <InputLabel htmlFor="my-input">Methodology</InputLabel>
               <Select
                 labelId="demo-simple-select-label"
                 id="demo-simple-select"
                 label="Methodology"
+                value={methodology}
+                onChange={handleMethodology}
               >
                 <MenuItem value={'Agile'}>Agile</MenuItem>
                 <MenuItem value={'Waterfall'}>Waterfall</MenuItem>
               </Select>
             </FormControl>
           </Grid>
-          <Grid xs={12}>
+          <Grid xs={4}>
             <FormControl>
               <InputLabel htmlFor="my-input">Developers</InputLabel>
-              <Input id="my-input" aria-describedby="my-helper-text" />
+              <Input
+                id="my-input"
+                aria-describedby="my-helper-text"
+                value={currValue}
+                onChange={handleChange}
+                onKeyDown={saveValue}
+              />
               <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
             </FormControl>
+          </Grid>
+          <Grid xs={8}>
+            <div className={"container"}>
+              {developers.map((item, index) => (
+                <Chip key={index} size="small" onDelete={() => handleDelete(item, index)} label={item} />
+              ))}
+            </div>
           </Grid>
         </Grid>
         <Button type="submit">Submit</Button>
