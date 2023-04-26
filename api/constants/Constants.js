76045070486194acb491b63a1490881257e505d2
dbarkowsky <dylanbarkowsky@gmail.com>
2023-04-26 12:48:52 -0700

    envs now work as intended

--- api/constants/Constants.js
+++ api/constants/Constants.js
@@ -1,3 +1,5 @@
+import 'dotenv/config.js';
+
 const Constants = {
   PORT: process.env.PORT || 3000,
   HOSTNAME: process.env.HOSTNAME || 'localhost'
--- app/src/constants/Constants.js
+++ app/src/constants/Constants.js
@@ -1,6 +1,6 @@
 const Constants = {
-  API_PORT: process.env.API_PORT || 3000,
-  HOSTNAME: process.env.HOSTNAME || 'localhost',
+  API_PORT: process.env.REACT_APP_API_PORT || 3000,
+  HOSTNAME: process.env.REACT_APP_HOSTNAME || 'localhost',
   VALID_NAME_SCHEMA: /^[a-zA-Z]{2,}(([',. -][a-zA-Z ])?[a-zA-Z ]*)*$/g,
   VALID_PRODUCT_NAME_SCHEMA: /^[a-zA-Z0-9]{2,}(([',. -][a-zA-Z0-9 ])?[a-zA-Z0-9 ]*)*$/g
 }
